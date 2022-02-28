e270737d4fab60560fc3e080f2c4c1122cd7dc66
Arshiya Kiani <zytell3301@gmail.com>
2022-03-01 01:18:11 +0330

    Temporarily print error message for easier debugging

--- default_reporter.go
+++ default_reporter.go
@@ -1,9 +1,12 @@
 package ErrorReporter
 
+import "fmt"
+
 type DefaultReporter struct {
 }
 
 func (d DefaultReporter) Report(err Error) {
+	fmt.Println(err)
 }
 
 func NewDefaultReporter() DefaultReporter {
